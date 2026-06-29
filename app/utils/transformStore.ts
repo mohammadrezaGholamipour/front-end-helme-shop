4d53ac7ae13e4743ccfc38131c866235adb9a494
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2026-06-29 21:35:09 +0330

    fix(ui):change icon and color

--- app/utils/transformStore.ts
+++ app/utils/transformStore.ts
@@ -9,7 +9,7 @@ export const transformStore = (data: StoreOut): StoreUI => {
             { icon: 'bale', link: data.bale || '', name: 'بله' },
             { icon: 'eita', link: data.eita || '', name: 'ایتا' },
             { icon: 'rubika', link: data.rubika || '', name: 'روبیکا' },
-            { icon: 'phone', link: data.phone || '', name: 'شماره تماس' },
+            { icon: 'phone', link: `tel:${data.phone.split(' - ')[0]}` || '', name: 'شماره تماس' },
             { icon: 'website', link: 'https://www.sohangaz.com', name: 'وبسایت' },
         ].filter(item => item.link),
         logo: '/images/helma-logo.webp',
