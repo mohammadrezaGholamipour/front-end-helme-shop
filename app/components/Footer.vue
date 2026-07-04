0bce8d69568d95bcc84e73d47fc3bdead318a829
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2026-07-04 21:00:44 +0330

    feat(footer):new footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -2,7 +2,88 @@
 const globalStore = useGlobalStore();
 </script>
 <template>
-  <footer
+  <footer  v-animate-stagger="{ delay: 500, threshold: 0.1, once: true }" class="parent-footer">
+    <div class="parent-footer__map">
+      <div>
+        <p>ما را پیدا کنید</p>
+        <Icon name="tabler:map-pin" class="w-6 h-6 text-[--gold-two]" />
+      </div>
+      <p>{{ globalStore.state.store?.address }}</p>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d821.5757134180984!2d50.79468376962758!3d34.54588660294193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f93a35a3182c78b%3A0x569309d4a8cfd093!2sShahab%20Danesh%20University!5e0!3m2!1sen!2s!4v1781537845333!5m2!1sen!2s"
+        style="border: 0"
+        loading="lazy"
+        class="rounded-lg shadow-md"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+    <section>
+      <div class="parent-footer__logo">
+        <img src="/images/helma-logo.webp" width="250" alt="سوهان و گز حلما" />
+      </div>
+      <div class="parent-social-media">
+        <div>
+          <p>لیست شبکه های اجتماعی حلما شاپ</p>
+          <Icon name="tabler:users" class="w-6 h-6 !text-[--gold-two]" />
+        </div>
+        <div>
+          <template v-for="item in globalStore.state.store?.social">
+            <a
+              v-if="item.name !== 'شماره تماس' && item.name !== 'وبسایت'"
+              class="parent-social-media__social-icon"
+              :href="item.link"
+              :key="item.link"
+              target="_blank"
+            >
+              <img :src="`/images/${item?.icon}.webp`" width="40" alt="" />
+            </a>
+          </template>
+          
+        </div>
+      </div>
+    </section>
+    <div class="parent-footer__quick-access">
+      <div>
+        <Icon name="tabler:bolt" class="w-6 h-6 text-[--gold-two]" />
+        <p>دسترسی سریع</p>
+      </div>
+      <div>
+        <Icon name="tabler:home" class="w-6 h-6 text-[--gold-two]" />
+        <p>صفحه اصلی</p>
+      </div>
+      <div>
+        <Icon name="tabler:info-circle" class="w-6 h-6 text-[--gold-two]" />
+        <p>درباره ما</p>
+      </div>
+      <div>
+        <Icon name="tabler:article" class="w-6 h-6 text-[--gold-two]" />
+        <p>وبلاگ</p>
+      </div>
+    </div>
+
+    <div class="parent-footer__contact-us">
+      <div>
+        <Icon name="tabler:phone-call" class="w-6 h-6 text-[--gold-two]" />
+        <p>تماس باما</p>
+      </div>
+      <div>
+        <Icon name="tabler:device-mobile" class="w-6 h-6 text-[--gold-two]" />
+        <p>09122510234</p>
+      </div>
+      <div>
+        <Icon
+          name="tabler:device-landline-phone"
+          class="w-6 h-6 text-[--gold-two]"
+        />
+        <p>09197402722</p>
+      </div>
+      <div>
+        <Icon name="tabler:clock-hour-3" class="w-6 h-6 text-[--gold-two]" />
+        <p>شنبه تا پنج شبنه 11 تا 22</p>
+      </div>
+    </div>
+  </footer>
+  <!-- <footer
     v-animate="{ type: 'blurIn', delay: 300, duration: 1000, once: true }"
   >
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -64,5 +145,5 @@ const globalStore = useGlobalStore();
         </template>
       </section>
     </div>
-  </footer>
+  </footer> -->
 </template>
