a94c55731ecaa2fe090c3ac97137f68b2c7dcf17
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2026-07-17 02:28:17 +0330

    feat(action):config github action for project

--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -4,9 +4,8 @@ export default defineNuxtPlugin(() => {
     const isServer = import.meta.server;
     const apiBaseClient = config.public.apiBase;
     const baseURL = isServer? 'http://backend:8000/helma-shop-api/v1' : apiBaseClient; 
-    console.log(isServer? 'http://backend:8000/helma-shop-api/v1' : apiBaseClient);
     const api = $fetch.create({
-      baseURL: apiBaseClient,
+      baseURL: baseURL,
       query: {
         application_id: '1',
       },
